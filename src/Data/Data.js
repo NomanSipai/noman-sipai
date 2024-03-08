f22ad6e76d50f1b43515f70e65ebf495fe8b0c5c
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:43:40 +0530

    adding data for projects page

---
+++ src/Data/Data.js
@@ -0,0 +1,176 @@
+const Data = [
+  {
+    name: "Boolean Blog",
+    img: "https://emreozturanli.vercel.app/images/boolean-blog.PNG",
+  },
+  {
+    name: "Contact App Firebase",
+    img: "https://emreozturanli.vercel.app/images/contact-app-firebase.PNG",
+  },
+  {
+    name: "Movie App Firebase",
+    img: "https://emreozturanli.vercel.app/images/movie-app-firebase.PNG",
+  },
+  {
+    name: "Recipe App",
+    img: "https://emreozturanli.vercel.app/images/recipe-app.PNG",
+  },
+  {
+    name: "Keeper App Firebase",
+    img: "https://emreozturanli.vercel.app/images/keeper-app.PNG",
+  },
+  {
+    name: "Dukkan",
+    img: "https://emreozturanli.vercel.app/images/dukkan.PNG",
+  },
+  {
+    name: "Meals",
+    img: "https://emreozturanli.vercel.app/images/meals.PNG",
+  },
+  {
+    name: "Countries-React",
+    img: "https://emreozturanli.vercel.app/images/countries-react.PNG",
+  },
+  {
+    name: "Users",
+    img: "https://emreozturanli.vercel.app/images/users.PNG",
+  },
+  {
+    name: "Task Tracker Axios",
+    img: "https://emreozturanli.vercel.app/images/task-tracker-axios.PNG",
+  },
+  {
+    name: "Task Tracker",
+    img: "https://emreozturanli.vercel.app/images/task-tracker.PNG",
+  },
+  {
+    name: "Weather Forecast",
+    img: "https://emreozturanli.vercel.app/images/weather-forecast.PNG",
+  },
+  {
+    name: "Cinetallinn",
+    img: "https://emreozturanli.vercel.app/images/cinetallinn.PNG",
+  },
+  {
+    name: "Languages",
+    img: "https://emreozturanli.vercel.app/images/languages.PNG",
+  },
+  {
+    name: "To-Do React",
+    img: "https://emreozturanli.vercel.app/images/to-do-react.PNG",
+  },
+  {
+    name: "Shopping List",
+    img: "https://emreozturanli.vercel.app/images/shopping-list.PNG",
+  },
+  {
+    name: "Navbar",
+    img: "https://emreozturanli.vercel.app/images/navbar.PNG",
+  },
+  {
+    name: "Tour Places",
+    img: "https://emreozturanli.vercel.app/images/tour-places.PNG",
+  },
+  {
+    name: "Tabs",
+    img: "https://emreozturanli.vercel.app/images/tabs.PNG",
+  },
+  {
+    name: "Color Generator",
+    img: "https://emreozturanli.vercel.app/images/color-generator.PNG",
+  },
+  {
+    name: "Slider",
+    img: "https://emreozturanli.vercel.app/images/slider.PNG",
+  },
+  {
+    name: "Menu",
+    img: "https://emreozturanli.vercel.app/images/menu.PNG",
+  },
+  {
+    name: "Accordion",
+    img: "https://emreozturanli.vercel.app/images/accordion.PNG",
+  },
+  {
+    name: "Tours",
+    img: "https://emreozturanli.vercel.app/images/tours.PNG",
+  },
+  {
+    name: "Random User",
+    img: "https://emreozturanli.vercel.app/images/random-user.PNG",
+  },
+  {
+    name: "To-Do App",
+    img: "https://emreozturanli.vercel.app/images/to-do-app.PNG",
+  },
+  {
+    name: "Countries",
+    img: "https://emreozturanli.vercel.app/images/countries.PNG",
+  },
+  {
+    name: "Calculator",
+    img: "https://emreozturanli.vercel.app/images/calculator.PNG",
+  },
+  {
+    name: "Guess the Number",
+    img: "https://emreozturanli.vercel.app/images/guess-the-number.PNG",
+  },
+  {
+    name: "Learn JS DOM",
+    img: "https://emreozturanli.vercel.app/images/learn-js-dom.PNG",
+  },
+  {
+    name: "Checkout",
+    img: "https://emreozturanli.vercel.app/images/checkout.PNG",
+  },
+  {
+    name: "Kids' Zoo",
+    img: "https://emreozturanli.vercel.app/images/kids-zoo.PNG",
+  },
+  {
+    name: "Simon Game",
+    img: "https://emreozturanli.vercel.app/images/simon-game.PNG",
+  },
+  {
+    name: "Day-n-Nite",
+    img: "https://emreozturanli.vercel.app/images/day-n-nite.PNG",
+  },
+  {
+    name: "Portfolio Template -2",
+    img: "https://emreozturanli.vercel.app/images/portfolio-website-template-2.PNG",
+  },
+  {
+    name: "Portfolio Template",
+    img: "https://emreozturanli.vercel.app/images/portfolio-website-template.PNG",
+  },
+  {
+    name: "Netflix Survey Form",
+    img: "https://emreozturanli.vercel.app/images/netflix.PNG",
+  },
+  {
+    name: "Portfolio SCSS",
+    img: "https://emreozturanli.vercel.app/images/portfolio-scss.PNG",
+  },
+  {
+    name: "Studeo",
+    img: "https://emreozturanli.vercel.app/images/studeo.PNG",
+  },
+  {
+    name: "Parallax Website",
+    img: "https://emreozturanli.vercel.app/images/parallax.PNG",
+  },
+  {
+    name: "Helicopter Animation",
+    img: "https://emreozturanli.vercel.app/images/helicopter-animation.PNG",
+  },
+  {
+    name: "Google",
+    img: "https://emreozturanli.vercel.app/images/google.PNG",
+  },
+  {
+    name: "Form Exercise",
+    img: "https://emreozturanli.vercel.app/images/form-exercise.PNG",
+  },
+];
+
+export default Data;
