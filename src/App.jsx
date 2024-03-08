7c8b3539d7c176d5cf283bacfe0332dd1f9e3f20
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:48:38 +0530

    importing project page in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Projects />
     </>
   );
 }
