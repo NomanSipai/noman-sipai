7e66c9e56cbad0e374358327438bc88f4e73bd47
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:22:50 +0530

    adding home to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Context/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>I'm Emre</div>
+      <div>
+        <p>I am a web developer with ambition, passion and good knowledge. I</p>
+        <p>am especially interested in the Frontend/Web Development, HTML,</p>
+        <p>CSS, JS, React.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
