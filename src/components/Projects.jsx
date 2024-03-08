b49ac3dd9b59259d45d5ba97e5c8675e80570fcc
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:58:41 +0530

    styling the card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,10 +2,10 @@ import Data from "../Data/Data";
 const Projects = () => {
   return (
     <div>
-      <div>
-        <div>Boolean Blog</div>
+      <div className=" bg-slate-50">
+        <div className=" text-3xl font-medium text-sky-600">{Data[0].name}</div>
         <div>
-          <img src="" alt="" />
+          <img className=" w-1/6 object-contain" src={Data[0].img} alt="" />
         </div>
         <div>
           <a href="">Source Code</a>
