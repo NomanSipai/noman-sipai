7e44b1ec958ae5a8364e7f08bdd6f4bb3db64b3a
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:37:01 +0530

    styling the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
 const Home = () => {
   return (
-    <div>
-      <div>I'm Emre</div>
-      <div>
-        <p>I am a web developer with ambition, passion and good knowledge. I</p>
-        <p>am especially interested in the Frontend/Web Development, HTML,</p>
-        <p>CSS, JS, React.</p>
+    <div className="flex flex-col items-center justify-center mt-16">
+      <div className=" text-9xl font-bold mb-20 font-sans">I'm Emre</div>
+      <div className="text-2xl text-center">
+        <p className="mb-2">
+          I am a web developer with ambition, passion and good knowledge. I
+        </p>
+        <p className="mb-2">
+          am especially interested in the Frontend/Web Development, HTML,
+        </p>
+        <p className="mb-2">CSS, JS, React.</p>
       </div>
     </div>
   );
