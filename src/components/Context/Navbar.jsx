c8745b2e2e378a94e2fd487fd8be3deefd2a511f
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:19:45 +0530

    styling the navbar

--- src/components/Context/Navbar.jsx
+++ src/components/Context/Navbar.jsx
@@ -1,12 +1,16 @@
 const Navbar = () => {
   return (
-    <div>
-      <div>emre</div>
+    <div className="flex justify-around items-center">
+      <div className="p-3 m-3 text-orange-500 text-4xl cursor-pointer font-bold">
+        emre
+      </div>
       <div>
-        <ul>
-          <li>Projects</li>
-          <li>About</li>
-          <li>Contact</li>
+        <ul className="flex text-xl font-medium">
+          <li className="p-3 m-3 hover:text-red-500 cursor-pointer">
+            Projects
+          </li>
+          <li className="p-3 m-3 cursor-pointer hover:text-red-500">About</li>
+          <li className="p-3 m-3 cursor-pointer hover:text-red-500">Contact</li>
         </ul>
       </div>
     </div>
