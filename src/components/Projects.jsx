e2f186d23a09efa0e01fe09200ad748b242640da
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 17:47:58 +0530

    creating cart in project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,18 @@
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>Boolean Blog</div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <a href="">Source Code</a>
+          <a href="">Visit Page</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
