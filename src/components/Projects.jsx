6c2460062128f3b2b8a5a054e5c58d85910f3d45
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-08 21:43:50 +0530

    styling card components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,25 @@
 import Data from "../Data/Data";
 const Projects = () => {
   return (
-    <div>
-      <div className=" bg-slate-50">
-        <div className=" text-3xl font-medium text-sky-600">{Data[0].name}</div>
-        <div>
-          <img className=" w-1/6 object-contain" src={Data[0].img} alt="" />
+    <div className=" flex ">
+      <div className=" bg-gray-100  flex flex-col items-center justify-center">
+        <div className=" text-3xl font-medium text-sky-600 mb-5">
+          {Data[0].name}
         </div>
         <div>
-          <a href="">Source Code</a>
-          <a href="">Visit Page</a>
+          <img
+            className=" w-1/4 object-contain block me-auto ms-auto"
+            src={Data[0].img}
+            alt=""
+          />
+        </div>
+        <div className="mt-8 text-orange-300">
+          <a className="me-20 border border-sky-600 font-medium p-2 cursor-pointer bg-white rounded">
+            Source Code
+          </a>
+          <a className=" border border-sky-600 font-medium p-2 cursor-pointer bg-white rounded">
+            Visit Page
+          </a>
         </div>
       </div>
     </div>
