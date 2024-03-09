254217abdb3147e1a4b271f8de1add6446c2ec4f
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-09 17:40:15 +0530

    adding contact page in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
