e2a9bc5edaf296b97277a2eb620d1488a93f70c1
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-09 17:59:07 +0530

    removing svg file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,11 @@
 import { Link } from "react-router-dom";
-import EmailIcon from "../assets/envelope.svg";
-import GithubIcon from "../assets/github.svg";
-import LinkedinIcon from "../assets/linkedin.svg";
 
 const Contact = () => {
   return (
     <div className="flex items-center justify-center mt-40">
-      <Link>
-        <img className=" w-10 m-10" src={EmailIcon} alt="email" />
-      </Link>
-      <Link>
-        <img className="w-10 m-10" src={GithubIcon} alt="github" />
-      </Link>
-      <Link>
-        <img className="w-10 m-10" src={LinkedinIcon} alt="linkedin" />
-      </Link>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
     </div>
   );
 };
