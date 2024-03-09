2e1e9218c09d74e2bb51fc33845a10cd357ba26c
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-09 17:48:00 +0530

    adding link tag

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import EmailIcon from "../assets/envelope.svg";
+import GithubIcon from "../assets/github.svg";
+import LinkdinIcon from "../assets/linkedin.svg";
+
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+    </div>
+  );
 };
 
 export default Contact;
