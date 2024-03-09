a3d2ff1212ce491d121a7bd12b764ce92b392ed6
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-09 17:38:50 +0530

    styling about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,40 @@
 const About = () => {
   return (
     <div>
-      <div>
+      <div className="w-3/5 mt-7 me-auto ms-auto">
+        <hr />
+      </div>
+      <div className="w-3/4 mt-7 me-auto ms-auto text-center text-xl">
+        <p className="mb-2">
+          My name is Emre. I am a <strong>Frontend Developer.</strong> I love
+          creating beautifull, usefull websites. I am always open to learn new
+        </p>
+        <p className="mb-2">
+          technologies. I always try to expand my tech stack. I also have an
+          extra interest in CSS. Playing with CSS is always fun
+        </p>
+        <p className="mb-5 ">and it has no borders.</p>
+        <p className=" underline underline-offset-1 mb-5 font-medium text-sky-600">
+          Some languages and tools that I use:
+        </p>
+        <ul className=" font-mono text-lg">
+          <li className="mb-3">React</li>
+          <li className="mb-3">JavaScript</li>
+          <li className="mb-3">Bootstrap</li>
+          <li className="mb-3">CSS</li>
+          <li className="mb-3">HTML</li>
+          <li className="mb-3">SASS</li>
+          <li className="mb-3">Styled Components</li>
+          <li className="mb-3">Material UI</li>
+          <li className="mb-3">Python</li>
+          <li className="mb-3">Firebase</li>
+          <li className="mb-3">Postman</li>
+          <li className="mb-3">Jira</li>
+          <li className="mb-3">Git</li>
+          <li className="mb-3">GitHub</li>
+        </ul>
+      </div>
+      <div className="w-3/5 mt-7 mb-20 me-auto ms-auto">
         <hr />
       </div>
     </div>
