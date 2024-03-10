671bdee2ed3b9f44a712f80460feb31370fac836
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-10 12:19:53 +0530

    styling hte bootstrap icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,15 @@ import { Link } from "react-router-dom";
 const Contact = () => {
   return (
     <div className="flex items-center justify-center mt-40">
-      <Link></Link>
-      <Link></Link>
-      <Link></Link>
+      <Link>
+        <i className="bi bi-envelope text-5xl m-10 text-sky-700"></i>
+      </Link>
+      <Link>
+        <i className="bi bi-github text-5xl m-10 text-sky-700"></i>
+      </Link>
+      <Link>
+        <i className="bi bi-linkedin text-5xl m-10 text-sky-700"></i>
+      </Link>
     </div>
   );
 };
