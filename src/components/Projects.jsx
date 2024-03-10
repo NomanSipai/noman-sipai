cec68b51ae9c01fb43fb04586b146f29cef87a75
nomanvaghela <nomanvaghela7@gmail.com>
2024-03-10 12:52:03 +0530

    background change in card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,10 @@ const Projects = () => {
     <div className="flex flex-wrap items-center justify-center w-3/4 me-auto ms-auto mt-24">
       {Data.map((item) => {
         return (
-          <div key={item.id} className="w-full md:w-1/3  p-4">
-            <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden border border-gray-200 shadow">
+          <div key={item.id} className="w-full md:w-1/3  p-4 ">
+            <div
+              style={{ backgroundColor: "#e5eef3" }}
+              className="max-w-md mx-auto rounded-xl overflow-hidden border border-gray-200 shadow shadow-amber-500">
               <div className="p-4 text-center font-bold text-3xl text-sky-500">
                 {item.name}
               </div>
