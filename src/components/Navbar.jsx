65ba7a186e4df29bbd3e25551b4c87f212385858
nomanvaghela <nomanvaghela7@gmail.com>
2024-04-09 15:58:05 +0530

    sdsd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,20 @@ const Navbar = () => {
       </div>
       <div>
         <ul className="flex text-xl font-medium">
-          <li className="p-3 m-3 hover:text-red-500 cursor-pointer">
-            <Link to="/projects">Projects</Link>
+          <li className="p-3 m-3 hover:text-red-500  cursor-pointer">
+            <Link className="focus:text-red-500" to="/projects">
+              Projects
+            </Link>
           </li>
-          <li className="p-3 m-3 cursor-pointer hover:text-red-500">
-            <Link to="/about">About</Link>
+          <li className="p-3 m-3 cursor-pointer hover:text-red-500 ">
+            <Link className="focus:text-red-500" to="/about">
+              About
+            </Link>
           </li>
-          <li className="p-3 m-3 cursor-pointer hover:text-red-500">
-            <Link to="/contact">Contact</Link>
+          <li className="p-3 m-3 cursor-pointer hover:text-red-500 ">
+            <Link className="focus:text-red-500" to="/contact">
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
