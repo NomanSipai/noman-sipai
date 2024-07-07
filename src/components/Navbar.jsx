39e6f1f43384c015a0262a3390b7600e7e494002
nomanvaghela <nomanvaghela7@gmail.com>
2024-07-07 13:20:40 +0530

    changes in images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-white fixed w-full border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white fixed z-10 w-full border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <NavLink
           to="/"
